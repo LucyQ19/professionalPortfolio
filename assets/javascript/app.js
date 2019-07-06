f4caa14e4106d6c639026332e1d03f8b4df6f31c
LucyQ19 <lucyq19@gmail.com>
2019-07-05 20:12:45 -0500

    updated app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,91 @@
 $(document).ready(function(){
-    $('.sidenav').sidenav();
+
+  $("#wordBtn").on("click", function(event) {
+  
+      event.preventDefault();
+  
+      $("#wordModal").modal("show");
+  
   });
+
+  $("#crystalBtn").on("click", function(event) {
+  
+      event.preventDefault();
+  
+      $("#crystalModal").modal("show");
+  
+  });
+
+  $("#triviaBtn").on("click", function(event) {
+  
+      event.preventDefault();
+  
+      $("#triviaModal").modal("show");
+  
+  });
+
+  $("#giphyBtn").on("click", function(event) {
+  
+      event.preventDefault();
+  
+      $("#giphyModal").modal("show");
+  
+  });
+
+  $("#trainBtn").on("click", function(event) {
+  
+      event.preventDefault();
+  
+      $("#trainModal").modal("show");
+  
+  });
+
+  $("#musicBtn").on("click", function(event) {
+  
+      event.preventDefault();
+  
+      $("#musicModal").modal("show");
+  
+  });
+
+  $("#liriBtn").on("click", function(event) {
+  
+      event.preventDefault();
+  
+      $("#liriModal").modal("show");
+  
+  });
+
+  $("#BAMazonBtn").on("click", function(event) {
+  
+      event.preventDefault();
+  
+      $("#BAMazonModal").modal("show");
+  
+  });
+
+  $("#friendBtn").on("click", function(event) {
+  
+      event.preventDefault();
+  
+      $("#friendModal").modal("show");
+  
+  });
+
+  $("#burgerBtn").on("click", function(event) {
+  
+      event.preventDefault();
+  
+      $("#burgerModal").modal("show");
+  
+  });
+
+  $("#petBtn").on("click", function(event) {
+  
+      event.preventDefault();
+  
+      $("#petModal").modal("show");
+  
+  });
+
+});
